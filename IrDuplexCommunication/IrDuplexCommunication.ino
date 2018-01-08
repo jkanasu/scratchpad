ce1746c09eeac16562858ab7e17e268fbbf3bfa2
jkanasu <jkanasu@gmail.com>
2018-01-08 07:10:16 +0530

    Restructuring to including RingBuffer

--- IrDuplexCommunication/IrDuplexCommunication.ino
+++ IrDuplexCommunication/IrDuplexCommunication.ino
@@ -1,6 +1,6 @@
-/* send.ino Example sketch for IRLib2
- *  Illustrates how to send a code.
- */
+// jagi has changed this script
+
+#include <RingBufCPP.h>
 #include <IRLibDecodeBase.h>
 #include <IRLibSendBase.h>    // First include the send base
 //Now include only the protocols you wish to actually use.
@@ -74,6 +74,16 @@ typedef struct{
 unsigned int DEVICE_IDENTITY=0;
 boolean MASTER_MODE=false;
 
+typedef struct {
+  byte protocolNum;
+  unsigned long timestamp;
+  unsigned long value;
+} NecIrSignal;
+#define MAX_NUM_NECSIGNALS_BUF 10
+//NECSIGNAL gIRSignalsNEC[MAX_NUM_NECSIGNALS];
+// Stack allocate the buffer to hold IR signals structs
+RingBufCPP<NecIrSignal, MAX_NUM_NECSIGNALS_BUF> buf;
+
 void setup() {
   Serial.begin(9600);
   delay(2000); while (!Serial); //delay for Leonardo
@@ -86,6 +96,7 @@ void setup() {
   Serial.println(F("Ready to receive IR signals"));
 }
 
+boolean fullDetail=false;
 void loop() {
   processSerialCommands();
   if(MASTER_MODE) deviceMonitoring();
@@ -109,7 +120,7 @@ typedef struct{
 } DeviceHBStatus;
 #define HB_INTERVAL 10000 // 10 seconds 
 #define HB_TIMEOUT_PERIOD 2000 // 2 second
-#define MAX_NUMBER_OF_DEVICES 4
+#define MAX_NUMBER_OF_DEVICES 8
 DeviceHBStatus hbStatusOfDevices[MAX_NUMBER_OF_DEVICES+1];
 byte gCurrentDeviceId = 1; // indexing can all go wrong keep note
 unsigned long gHBsignalNumber = 0;
@@ -118,14 +129,23 @@ void deviceMonitoring(){
     gCurrentDeviceId = 1;
     Serial.println(F("Restarting from beginning"));
   }
-  DeviceHBStatus& currentDeviceStatus = hbStatusOfDevices[gCurrentDeviceId];
+  monitorDevice(gCurrentDeviceId);
+  Serial.print(F("Device Status ")); Serial.print(gCurrentDeviceId); Serial.print(F(" : "));Serial.println(hbStatusOfDevices[gCurrentDeviceId].hbStatus); 
+  if(hbStatusOfDevices[gCurrentDeviceId].hbStatus == HB_SUCCESS ||
+  hbStatusOfDevices[gCurrentDeviceId].hbStatus == HB_FAILED){
+    gCurrentDeviceId++;
+  }
+}
+
+void monitorDevice(byte currentDeviceId){
+  DeviceHBStatus& currentDeviceStatus = hbStatusOfDevices[currentDeviceId];
   if(currentDeviceStatus.hbStatus == HB_NOT_SENT){
     gHBsignalNumber++;
-    sendHBRequest(gCurrentDeviceId);
+    sendHBRequest(currentDeviceId);
     currentDeviceStatus.hbStatus = HB_RESPONSE_WAITING;
     currentDeviceStatus.lastEventTime = millis();
-    //Serial.print(F("HB request sent "));Serial.println(gCurrentDeviceId, DEC);
-    return;
+    Serial.print(F("HB request sent "));Serial.println(currentDeviceId, DEC);
+    goto monitorDeviceWorkFinish;//return;
   }
   if(currentDeviceStatus.hbStatus == HB_RESPONSE_WAITING){
     // someone else is expected to change this status
@@ -134,26 +154,25 @@ void deviceMonitoring(){
     if(waitingTime > HB_TIMEOUT_PERIOD){
       // donot put any delay here, the main loop() may need that time to do some work
       currentDeviceStatus.hbStatus = HB_FAILED;
-      Serial.print(F("HB timedout "));Serial.println(gCurrentDeviceId, DEC);
-      return;
+      Serial.print(F("HB timedout "));Serial.println(currentDeviceId, DEC);
+      goto monitorDeviceWorkFinish;//return;
     }
     processIRSignals();
-    return;
+    goto monitorDeviceWorkFinish;//return;
   }
   if(currentDeviceStatus.hbStatus == HB_FAILED){
-    Serial.print(F("HB Failed "));Serial.println(gCurrentDeviceId, DEC);
+    Serial.print(F("HB fail "));Serial.println(currentDeviceId, DEC);
     currentDeviceStatus.hbStatus = HB_NOT_SENT;
-    gCurrentDeviceId++;
-    return;
+    goto monitorDeviceWorkFinish;//return;
   }
   if(currentDeviceStatus.hbStatus == HB_SUCCESS){
-    Serial.print(F("HB succeeded "));Serial.println(gCurrentDeviceId, DEC);
+    Serial.print(F("HB success "));Serial.println(currentDeviceId, DEC);
     currentDeviceStatus.hbStatus = HB_NOT_SENT;
-    gCurrentDeviceId++;
     //unsigned long lastHBActivityTime = findElapsedTimeInMillis(currentDeviceStatus.lastEventTime);//millis() - currentDeviceStatus.lastEventTime;
-    return;
+    goto monitorDeviceWorkFinish;//return;
   }
-  Serial.print(F("HB status "));Serial.println(currentDeviceStatus.hbStatus, DEC);
+monitorDeviceWorkFinish:
+  Serial.print(F("Monitor HB status "));Serial.print(currentDeviceId, DEC); Serial.print(F(" : "));Serial.println(currentDeviceStatus.hbStatus, DEC);
 }
 
 void processSerialCommands(){
@@ -163,28 +182,70 @@ void processSerialCommands(){
   //Serial.print(F("command received "));Serial.println(inComingCommand, DEC);
   switch(inComingCommand){
     case 49: // 1 in ascii
-      Serial.println(F("Send signal"));
-      processSerialSendRequest();
+      processSerial1();
       break;
     case 50: // 2 in ascii
-      Serial.println(F("Receive signal"));
-      processSerialReceiveRequest();
+      processSerial2();
+      break;
+    case 51: // 3 in ascii
+      break;
+    case 56: // 8 in ascii
+      processSerial8();
+      break;
+    case 57: // 9 in ascii
+      processSerial9();
+      break;
+    case 68: // D in ascii
+    case 100: // d in ascii
+      deviceMonitoring();
+      break;
+    case 69: // E in ascii
+    case 101: // e in ascii
+      processSerialE();
+      break;
+    case 70: // F in ascii
+    case 102: // f in ascii
+      processSerialF();
       break;
     default:
       Serial.print(F("Unknown serial signal ")); Serial.print(inComingCommand,DEC);Serial.print(F("-"));Serial.println(inComingCommand,HEX);
-      identifySelf();
+      processSerialX();
       break;
   }
 }
 
-void processSerialSendRequest(){
+void processSerial1(){
+  Serial.println(F("Send signal"));
   sendHBRequest(gCurrentDeviceId);
   gCurrentDeviceId++;
   if(gCurrentDeviceId > MAX_NUMBER_OF_DEVICES) gCurrentDeviceId=1;
 }
 
-void processSerialReceiveRequest(){
+void processSerial2(){
+  Serial.println(F("Receive signal"));
   processIRSignals();
+}
+
+void processSerial8(){
+  print_buf_contents();
+}
+
+void processSerial9(){
+  identifySelf();
+}
+
+void processSerialE(){
+  dump_buf_contents();
+}
+
+void processSerialF(){
+  Serial.println(F("Change dump level"));
+  fullDetail = (!fullDetail);
+}
+
+void processSerialX(){
+  Serial.println(F("Print Stats"));
+  printIrSignalStats();
 }
 
 void processIRSignals(){
@@ -199,7 +260,8 @@ void processIRSignals(){
 #define HB_REQUEST_CODE_PARAMS 0x00
 void sendHBRequest(byte currentDeviceId){
   unsigned long hbRequestCode = formatCodeForSendingRequest(currentDeviceId,REQUEST_HB,gHBsignalNumber);
-  mySender.send(NEC,hbRequestCode,32);
+  //mySender.send(NEC,hbRequestCode,32);
+  sendIRSignals(hbRequestCode);
   Serial.print(F("HB Request Signal sent "));Serial.println(hbRequestCode, HEX);
   //delay(1000);
 }
@@ -207,7 +269,8 @@ void sendHBRequest(byte currentDeviceId){
 #define HB_RESPONSE_CODE_PARAMS 0x00
 void sendHBResponse(JIRCode& jIrCode){
   unsigned long hbResponseCode = formatCodeForSendingResponse(jIrCode.sourceAddress,RESPONSE_HB,jIrCode.parameterData);
-  mySender.send(NEC,hbResponseCode,32);
+  //mySender.send(NEC,hbResponseCode,32);
+  sendIRSignals(hbResponseCode);
   Serial.print(F("HB Response Signal sent "));Serial.println(hbResponseCode, HEX);
 }
 
@@ -224,7 +287,7 @@ void processHBResponse(JIRCode& jIrCode){
 }
 
 void processJIRSignals(JIRCode& jIrCode){
-  //Serial.println(F("processJIR Signals "));
+  Serial.println(F("processJIR Signals "));
   jIrCode = convertToJIRCodeFromRawDecodedValue(jIrCode);
   if(jIrCode.protocolNum != NEC || jIrCode.destinationAddress != DEVICE_IDENTITY){
     processJIRSignalsAddressedToOthers(jIrCode);
@@ -258,26 +321,166 @@ unsigned long numOfRawValidIRSignalsReceived = 0;
 unsigned long numOfValidJIRSignalsReceived = 0;
 boolean receiveValidJIRSignal(JIRCode& jIRCode){
   boolean retVal = false;
-  if (myReceiver.getResults()) {
-    numOfRawIRSignalsReceived++;
-    //Serial.print(F("numOfRawIRSignalsReceived ")); Serial.println(numOfRawIRSignalsReceived, DEC);
-    if(myDecoder.decode()) {
-      //myDecoder.dumpResults(false);  //Now print results. Use true for more detail
-      numOfRawValidIRSignalsReceived++;
-      //Serial.print(F("numOfRawValidIRSignalsReceived ")); Serial.println(numOfRawValidIRSignalsReceived, DEC);
-      if(myDecoder.protocolNum == NEC || myDecoder.protocolNum == RC5){
-        numOfValidJIRSignalsReceived++;
-        jIRCode.protocolNum = myDecoder.protocolNum;
-        jIRCode.value = myDecoder.value; // assign the value to the input reference only when a valid JIR signal
-        //Serial.print(F("numOfValidJIRSignalsReceived ")); Serial.println(numOfValidJIRSignalsReceived, DEC);
-        retVal=true;
-      }
-    } else Serial.println(F("Decode failed"));
-    delay(FORCED_DELAY_TO_DEBOUNCE_SIGNALS);
-    // For RC5 the signal is detected twice, donot know why.
-    myReceiver.enableIRIn();      //Immediately Restart receiver, the debug print is long afterwards
-  }
+  receiveIRSignals();
+  if(buf.isEmpty()) return retVal;
+  NecIrSignal necIrSignal;
+  if(!buf.pull(&necIrSignal)) return retVal;
+  jIRCode.protocolNum = necIrSignal.protocolNum;
+  jIRCode.value = necIrSignal.value;
+  retVal = true;
   return retVal;
+}
+
+unsigned long rawIRSignalsSent = 0;
+unsigned long rawIRSignalsEchoFailed = 0;
+unsigned long rawIRSignalsReTransmitted = 0;
+unsigned long recentTransmitted = 0xFFFFFFFF;
+boolean isWaitingForEcho = false;
+boolean isEchoReceived = false;
+boolean isReTransmissionRequired = false;
+#define ECHO_TIMEOUT_PERIOD 2000 // In milliseconds 125 // For NEC standard, the transmission period is upto 110 ms
+#define NUM_IRSIGNAL_MIN_RETRANSMITS 2
+#define NUM_IRSIGNAL_MAX_RETRANSMITS 4
+void sendIRSignals(unsigned long iRcode){
+  Serial.print(F("Send raw Ir Signal "));Serial.println(iRcode, HEX);
+  recentTransmitted = 0xFFFFFFFF;
+  isWaitingForEcho = false;
+  isEchoReceived = false;
+  isReTransmissionRequired = true;
+  //recentTransmitted = iRcode;
+  //mySender.send(NEC,iRcode,32);
+  //isWaitingForEcho = true;
+  //receiveIRSignals();
+  byte i = 0;
+  unsigned long sendTimeStamp = millis();
+  do{
+    if(isReTransmissionRequired == true){
+      i++;
+      recentTransmitted = iRcode;
+      unsigned long sendTimeStampBefore = millis();
+      mySender.send(NEC,iRcode,32);
+      sendTimeStamp = millis();
+      long timeTakenToSend = sendTimeStamp - sendTimeStampBefore;
+      Serial.print(F(" Tx attempt ")); Serial.print(i);Serial.print(F(" : "));
+      Serial.print(sendTimeStamp); Serial.print(F(" - "));Serial.print(sendTimeStampBefore); Serial.print(F(" = "));Serial.println(timeTakenToSend,DEC);
+      rawIRSignalsSent++;
+      isWaitingForEcho = true;
+      isReTransmissionRequired = false;
+    }
+    while(true){
+      receiveIRSignals();
+      if(isEchoReceived){
+        //Serial.println(F("Echo Received"));
+        break;
+      }
+      if(hasTimedOut(sendTimeStamp, ECHO_TIMEOUT_PERIOD)){
+        Serial.print(millis()); Serial.println(F(" Echo Timed Out"));
+        isReTransmissionRequired = true;
+        rawIRSignalsEchoFailed++;
+        rawIRSignalsReTransmitted++;
+        break;
+      }
+    }
+    if(isEchoReceived ) {
+      if(i < NUM_IRSIGNAL_MIN_RETRANSMITS) {
+        isReTransmissionRequired = true;
+        rawIRSignalsReTransmitted++;
+        //Serial.print(F("Force Tx attempt ")); Serial.println(i);
+        //delay(200);// else we may simply collide with our selves, actually NO :-(
+        continue;
+      }
+      break;
+    }
+  }while(i < NUM_IRSIGNAL_MAX_RETRANSMITS);
+  if(isEchoReceived) {
+    Serial.print(F("Raw Send Signal complete "));Serial.println(iRcode, HEX);
+  }
+}
+
+unsigned long rawIRSignalsReceived = 0;
+unsigned long rawIRSignalsDecodeFailures = 0;
+unsigned long rawIRSignalsDuplicates = 0;
+unsigned long rawIRSignalsDropped = 0;
+void receiveIRSignals(){
+  //Continue looping until you get a complete signal received
+  if (myReceiver.getResults()) { // get the Results
+    rawIRSignalsReceived++;
+    if (myDecoder.decode()){  //Decode it
+      //myDecoder.dumpResults(fullDetail);  //Now print results. Use false for less detail
+      NecIrSignal necIrSignal;
+      necIrSignal.protocolNum = myDecoder.protocolNum;
+      necIrSignal.value = myDecoder.value;
+      necIrSignal.timestamp = millis();
+      byte bufLength = buf.numElements();
+      if(isWaitingForEcho && necIrSignal.value == recentTransmitted){
+        isEchoReceived = true;
+        isWaitingForEcho = false;
+        //Serial.print(F("Echo Received ")); Serial.println(necIrSignal.value, HEX);
+        goto enableReceiver;
+      }
+      if(bufLength>0){
+        NecIrSignal* prevElement = buf.peek(bufLength-1);
+        //if(!prevElement) goto enableReceiver; // Needed only in multithreading or ISR
+        if(necIrSignal.value == 0xFFFFFFFF || necIrSignal.value == prevElement->value) {
+          //Serial.print(F("Duplicate found ")); Serial.println(necIrSignal.value, HEX);
+          rawIRSignalsDuplicates++;
+          goto enableReceiver;
+        }
+      }
+      if(!buf.add(necIrSignal)) {    // Add it to the buffer
+        Serial.print(F("Q failed ")); Serial.println(necIrSignal.value, HEX);
+        rawIRSignalsDropped++;
+      }
+    } else { // This is an indication of possible collision or a simple hardware problem, hence re-transmit
+      Serial.println(F("Decode failed"));
+      rawIRSignalsDecodeFailures++;
+      if(isWaitingForEcho){
+        rawIRSignalsEchoFailed++;// = 0;
+        isReTransmissionRequired = true;
+        Serial.print(F("Raw Echo failed for ")); Serial.println(recentTransmitted, HEX);
+        goto enableReceiver;
+      }
+    }
+enableReceiver:
+    myReceiver.enableIRIn();      //Restart receiver
+  }
+}
+
+// Print the buffer's contents then empty it
+void print_buf_contents()
+{
+  NecIrSignal * e = 0;
+  Serial.println("\n______Peek contents of ring buffer_______");
+  // Keep looping until pull() returns NULL
+  for (int i = 0 ; i<buf.numElements(); i++) {
+    e = buf.peek(i);
+    if(!e) break;
+    Serial.print(F("t ")); Serial.print(e->timestamp);
+    Serial.print(F(" v ")); Serial.println(e->value, HEX);
+  }
+  Serial.println("______Done peeking contents_______");
+}
+
+// Print the buffer's contents then empty it
+void dump_buf_contents()
+{
+  NecIrSignal e;
+  Serial.println("\n______Dumping contents of ring buffer_______");
+  // Keep looping until pull() returns NULL
+  while (buf.pull(&e))
+  {
+    Serial.print(F("t ")); Serial.print(e.timestamp);
+    Serial.print(F(" v ")); Serial.println(e.value, HEX);
+  }
+  Serial.println("______Done dumping contents_______");
+}
+
+void printIrSignalStats(){
+  Serial.println(F("RawTx\tEchoFail\tReTransmitted"));
+  Serial.print(rawIRSignalsSent);Serial.print(F("\t"));Serial.print(rawIRSignalsEchoFailed);Serial.print(F("\t"));Serial.println(rawIRSignalsReTransmitted);
+  Serial.println(F("RawRx\tDecodeFail Duplicate Dropped"));
+  Serial.print(rawIRSignalsReceived,DEC);Serial.print(F("\t"));Serial.print(rawIRSignalsDecodeFailures,DEC);Serial.print(F("\t"));
+  Serial.print(rawIRSignalsDuplicates,DEC);Serial.print(F("\t"));Serial.println(rawIRSignalsDropped,DEC);
 }
 
 // this is used to blink the identity led
@@ -355,15 +558,25 @@ void printSerialJIRCode(JIRCode& jIrCode){
   Serial.print(jIrCode.command,HEX);Serial.print(F("-"));Serial.println(jIrCode.parameterData,HEX);
 }
 
-unsigned long findElapsedTimeInMillis(unsigned long& lastEventTime){
+// TODO Need to check so that the overflow doesnot cause too much time to timeout
+#define TIME_OVERFLOW_VALUE 0xFFFFFFFF
+unsigned long findElapsedTimeInMillis(const unsigned long& lastEventTime){
   unsigned long retVal;
   unsigned long currentTime = millis();
   retVal = currentTime - lastEventTime;
-  if(retVal < 0 ) {
-    retVal = (0xFFFFFFFE - lastEventTime) + currentTime;
+  if(retVal < 0 ) { // Overflow is automatically taken care with unsigned numbers, we need not do this.
+    retVal = (TIME_OVERFLOW_VALUE - lastEventTime) + currentTime;
   }
   return retVal;
 }
+
+boolean hasTimedOut(const unsigned long& lastEventTime, const unsigned long& timeOutPeriod){
+  long timeRemaining = timeOutPeriod - findElapsedTimeInMillis(lastEventTime);
+  //Serial.print(F("timeRemaining "));Serial.println(timeRemaining);
+  if (timeRemaining > 0 ) return false;
+  return true;
+}
+
 
 #define pinMasterMode  A5
 
